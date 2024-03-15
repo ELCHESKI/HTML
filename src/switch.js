a90ef76bc70a75620d062f33463c3eb0b20be65f
aNDREY <josminmunoz015@gmail.com>
2024-03-15 10:50:57 -0600

    Tarea terminada

--- src/switch.js
+++ src/switch.js
@@ -1,35 +1,72 @@
 document.querySelector('#header').innerHTML = "estoy leyendo el documento switch"
-var fruta
+var Dia
 var mensaje 
-var userInput = prompt("ingrese el numero de la fruta")
+var userInput = prompt("cual dia de la semana")
 
-function jugo_de_fresas() {
-    mensaje = ("batir fresas")
+function lunes() {
+    mensaje = ("odio los lunes")
     document.querySelector('#header').innerHTML = mensaje
   }
-function jugo_de_naranja() {
-    window.alert("batiendo naranjas")
-    mensaje = "batir naranjas y mandarinas"
+function martes() {
+    mensaje = "Hoy es martes de moira"
     document.querySelector('#header').innerHTML = mensaje
   }
 
-  function servir_vaso_de_agua() {
-    window.alert('sirveme el vaso de agua')
-    mensaje = "servirme vaso con agua"
+  function miercoles() {
+    mensaje = "hoy es miercoles dia de mr"
     document.querySelector('#header').innerHTML = mensaje
   }
+
+  function jueves() {
+    mensaje = "feliz jueves¡¡!!"
+    document.querySelector('#header').innerHTML = mensaje
+  }  
+
+  function viernes() {
+    mensaje = "hoy es viernes cuidado que se te pierde"
+    document.querySelector('#header').innerHTML = mensaje
+  }  
+
+  function sabado() {
+    mensaje = "hoy es sabado por arriba y por abajo me la ando descansando"
+    document.querySelector('#header').innerHTML = mensaje
+  }  
+
+  function Domingo() {
+    mensaje = "hoy es domingo pegate un brinco porque mañana no va a ser lindo"
+    document.querySelector('#header').innerHTML = mensaje
+  }  
+
+  function Dia_invalido() {
+    mensaje = "es duro pero nada mas hay siete dias si no sabias"
+    document.querySelector('#header').innerHTML = mensaje
+  }  
   
 fruta = parseInt(userInput)// con paserInt se convierte a entero
 
 switch(fruta) {
     case 1:
-        jugo_de_fresas()
+        lunes()
     break
     case 2:
+        martes()
+    break    
     case 3:
-        jugo_de_naranja()
+        miercoles()
     break
+    case 4:
+        jueves()
+    break
+    case 5:
+        viernes()
+    break
+    case 6:
+        sabado()
+    break
+    case 7: 
+        Domingo()
+    break       
     default:
-        servir_vaso_de_agua()  
+        Dia_invalido()  
     break 
 }
