fa0fca496af009f06c23aee0f44f3cafc5a01d6f
aNDREY <josminmunoz015@gmail.com>
2024-04-25 11:11:17 -0600

    proyecto

--- src/canasta.js
+++ src/canasta.js
@@ -1,16 +1,18 @@
 document.querySelector('#header').innerHTML = " estoy leyendo el elemento canasta"
 
-var canasta = [""]
-
+var canasta = []
+var i = 0
 
 do{
 
+  var respuesta = prompt("1.ver los elementos de la cesta\n2. Agregar una fruta a la cesta\n3. quitar ultima fruta de la cesta")  
 
 
 
 
 
-}while()
+
+}while(i canasta.length)
 
 
 
