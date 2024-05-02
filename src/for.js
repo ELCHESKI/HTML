c757a664a95c7f991d88f11e1e2becbce7236e22
aNDREY <josminmunoz015@gmail.com>
2024-05-02 10:34:52 -0600

    multiplicacion 1 al 10

---
+++ src/for.js
@@ -0,0 +1,18 @@
+document.querySelector('#header').innerHTML = "sjc"
+
+
+
+
+
+for (let index = 0; index < 11; index++) {
+    for (let index1 = 0; index1 < 11; index1++){
+    var result = index*index1
+    console.log(index, "x", index1 , "=", result) 
+    }        
+ 
+}
+
+
+
+
+
